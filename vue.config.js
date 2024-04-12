2ff6e7318733d29c9d63d1426f25fef6cf0280fc
wang.wangqiang <wang.wangqiang@zhaopin.com.cn>
2024-04-12 17:18:53 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     proxy: {
       "^/firewall": {
-        target: "http://127.0.0.1:8080",
+        target: "http://172.17.0.4:8080",
         changeOrigin: true
       }
     },
